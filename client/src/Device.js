15f4ad0dda7900ac05cf261df8a72acb6e0eeb60
Edward Kesicki <edkesicki@gmail.com>
2021-12-20 16:49:57 -0500

    Moved API Calls to Device
    Moved fetch calls to the device component, and removed initial 'greetings' training code.

--- client/src/Device.js
+++ client/src/Device.js
@@ -1,13 +1,53 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 // We want each grab bag item to be its own element.
-// Created an element successfully. Now try to create them in a loop
-// Example: Have a list of greetings, go through the list and 
-//   generate each greeting. 
+// Want to be able to have each device make its own
+// fetch - this way we can move the logic here and 
+// have the App just load more devices when it needs to
 
 
-function Device ({greeting}) {
-    return <div className = "device">{greeting}, here is a device. </div>;
+function Device () {
+    // initializing the state as empty 
+    const [device, setDevice] = React.useState('');
+
+    // useEffect will run when the component first mounts,
+    // then we can set the state with that data.
+    // This is where we want to fetch a device.
+    useEffect(() => {
+
+        async function getDevice () {
+            try {
+                // we will wait for the fetch request, then set its response as our data
+                // TODO: This request will get the same thing each time - the first entry 
+                //       Need to figure out a way to grab them sequentially
+                const res = await fetch("https://www.ifixit.com/api/2.0/wikis/CATEGORY?offset=17&limit=1");
+                const data = await res.json();
+
+                /* console.log("getDevice returned the following data:");
+                console.log(typeof(data));
+                console.log(data); */
+
+                // set this component's state using the data we just got
+                // the device ojbect itself is at data[0]
+                setDevice(data[0]);
+
+            } catch (err) {
+                console.log(err);
+            }
+        }
+
+        // Above code is writing the function, now we call it
+        getDevice();
+        console.log(device.image.standard); //<img src = {device.image.standard}></img>
+
+
+    }, []); // not dependent on anything, we don't want this component to change 
+
+    return (
+        <div className = "device" key = {device.wikiid} >
+            <label>Display Title: {device.display_title}</label>
+        </div>
+    );
 }
 
 
--- client/src/index.js
+++ client/src/index.js
@@ -4,38 +4,52 @@ import './index.css';
 import Device from './Device';
 import reportWebVitals from './reportWebVitals';
 
-const allGreetings = [
-  {id: "a", value: "Hello"},
-  {id: "b", value: "Howdy"},
-  {id: "c", value: "What's up"},
+const allDevices = [
+  {wikiid: '0', display_title: "dummy_1"}
 ]
 
+
 function App () {
-  const [greetings, setGreetings] = React.useState(allGreetings);
+
+  //const [greetings, setGreetings] = React.useState(allGreetings);
+
+  const [devices, setDevices] = React.useState(allDevices);
+  
 
   fetch("https://www.ifixit.com/api/2.0/wikis/CATEGORY?offset=0&limit=10")
-    .then(response => response.json(0))
-    .then(data => {
-      /*here is where we can manipulate the returned data*/
-      //const currentDevices = data.keys();
-      //console.log(typeof(currentDevices));
-      //console.log(currentDevices);
-      data.forEach(deviceEntry => {
-        console.log(deviceEntry.display_title);
-      });
+  .then(response => response.json(0))
+  .then(data => {
+    // here is where we can manipulate the returned data
+    data.forEach(deviceEntry => {
+      //console.log(deviceEntry.display_title);
+      allDevices.push(deviceEntry);
     });
+
+    //console.log("Here is the allDevices array: ");
+    allDevices.forEach(deviceEntry => {
+      // console.log(deviceEntry);
+    });
+  });
+
 
   return (
     <div>
+      <Device></Device>
+      <Device></Device>
+      <Device></Device>
+    </div>
+  );
+
+/*
+    <div>
       <ul>
-        {greetings.map(curGreeting => (
-          <Device key = {curGreeting.id} greeting = {curGreeting.value}>
+        {allDevices.map(currentDevice => (
+          <Device device = {currentDevice}>
           </Device>
         ))}
       </ul>
     </div>
-  );
-
+*/
 }
   
 ReactDOM.render(
