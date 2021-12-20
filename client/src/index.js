b6f0d8ccd0a5e2b1c66cc587837a713a86fb4ca4
Edward Kesicki <edkesicki@gmail.com>
2021-12-20 16:50:44 -0500

    Cleaned up unused code
    Cleaned up unused code in the App file that has been moved to the Device file

--- client/src/index.js
+++ client/src/index.js
@@ -4,32 +4,7 @@ import './index.css';
 import Device from './Device';
 import reportWebVitals from './reportWebVitals';
 
-const allDevices = [
-  {wikiid: '0', display_title: "dummy_1"}
-]
-
-
 function App () {
-
-  //const [greetings, setGreetings] = React.useState(allGreetings);
-
-  const [devices, setDevices] = React.useState(allDevices);
-  
-
-  fetch("https://www.ifixit.com/api/2.0/wikis/CATEGORY?offset=0&limit=10")
-  .then(response => response.json(0))
-  .then(data => {
-    // here is where we can manipulate the returned data
-    data.forEach(deviceEntry => {
-      //console.log(deviceEntry.display_title);
-      allDevices.push(deviceEntry);
-    });
-
-    //console.log("Here is the allDevices array: ");
-    allDevices.forEach(deviceEntry => {
-      // console.log(deviceEntry);
-    });
-  });
 
 
   return (
