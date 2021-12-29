a859b1864b8007375ccfb3dc02ee784d0e144c52
Edward Kesicki <edkesicki@gmail.com>
2021-12-29 16:33:47 -0500

    Infinite Scroll Works
    Moved Device Effects and state to the App file. Infinite Scrolling now works, but need to manually resize the window to get it to work. Currently just a column of devices, not a grid.

--- client/src/Device.js
+++ client/src/Device.js
@@ -6,44 +6,18 @@ import React, { useEffect } from "react";
 // have the App just load more devices when it needs to
 
 
-function Device () {
-    // initializing the state as empty 
-    const [device, setDevice] = React.useState([]);
-
-    async function getAndSetDevice () {
-        try {
-            // we will wait for the fetch request, then set its response as our data
-            // TODO: This request will get the same thing each time - the first entry 
-            //       Need to figure out a way to grab them sequentially
-            const res = await fetch("https://www.ifixit.com/api/2.0/wikis/CATEGORY?offset=17&limit=1");
-            const data = await res.json();
-
-            setDevice(data[0]);
-
-        } catch (err) {
-            console.log(err);
-        }
-    }
-
-    // useEffect will run when the component first mounts,
-    // then we can set the state with that data.
-
-    useEffect(() => {
-        getAndSetDevice();
-    },[]); // not dependent on anything - give an empty array so it only executes this once
+function Device (props) {
 
     // With the images, since they are nested info it looks like they are not loading in time
     // Need to have a temporary value in the img while the object loads?
     // Did this with the question mark in device.image?.standard.
     // I think the ? mark means that it will only load the .standard if there's something to load
 
-    //            <img src = {device.image.standard} alt = "No Image Available"></img>
-
     return (
-        <div className = "device" key = {device.wikiid}>
-            <img src = {device.image?.standard} alt></img>
-            <li>{device.display_title}</li>
-        </div>
+        <span className = "device" key = {props?.wikiid}>
+            <img src = {props.image?.standard} alt = "Error Loading Item"></img>
+            <li key = {props?.wikiid}>{props?.display_title}</li>
+        </span>
     );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -4,15 +4,72 @@ import './index.css';
 import Device from './Device';
 import reportWebVitals from './reportWebVitals';
 
+
+
+
 function App () {
 
 
+  const [deviceOffset, setDeviceOffset] = React.useState(0);
+
+  const [deviceList, setDeviceList] = React.useState([]);
+
+  const [keepLoading, setKeepLoading] = React.useState(true);
+
+  // handleScroll triggers when we scroll down the page
+  //   It calculates if we have scrolled past the bottom of the page
+  //   Then it sets keepLoading to True.
+  //   This will cause our useEffect to trigger and fetchAndSetOneDevice
+
+  const handleScroll = () => {
+    let userScrollHeight = window.innerHeight + window.scrollY;
+    let windowBottomHeight = document.documentElement.offsetHeight;
+    if (userScrollHeight >= windowBottomHeight) {
+      setKeepLoading(true);
+    }
+  };
+
+
+  // moving back to fetch.then.then notation, starting with just one device
+  const fetchAndSetOneDevice = async() => {
+    const requestAddress = 
+        "https://www.ifixit.com/api/2.0/wikis/CATEGORY?offset=" + deviceOffset + "&limit=1";
+
+    
+    fetch(requestAddress)
+    .then(r => r.json())
+    .then(response => {
+      setDeviceOffset((deviceOffset) => deviceOffset + 1);
+      setDeviceList((deviceList) => [...deviceList, response[0]]);
+      setKeepLoading((keepLoading) => false);
+    });
+
+  }
+
+  React.useEffect(() => {
+    // Will fetch one device and set state with that device
+    // Trying to make a custom device object
+    
+    window.addEventListener("scroll", handleScroll); // attaching scroll event listener
+
+
+  }, [keepLoading]); 
+
   return (
     <div>
-      <Device></Device>
+      {deviceList.length ? 
+        (deviceList.map(deviceEntry => {
+          console.log("In Mapping Function");
+          console.log(deviceList);
+          return <Device {...deviceEntry} key = {deviceEntry?.wikiid}></Device>
+        }))
+        : "No Items Loaded Yet"}
     </div>
   );
 
+/*
+  
+*/
 }
   
 ReactDOM.render(
